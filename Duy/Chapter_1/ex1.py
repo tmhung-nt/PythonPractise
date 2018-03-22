dd3261c98aff5e3f43d4651688bc9951a7538a19
TMA/DuyLe-Win10 <intern.lkduy@tma.com.vn>
2018-03-22 11:48:35 +0700

    Chapter_1

---
+++ Duy/Chapter_1/ex1.py
@@ -0,0 +1,12 @@
+print('Hello world!')
+print('What is your name?') 
+myName = input()  #myName = 'gia tri input vao'
+print('It is a good to meet you,' + myName)
+print('The length of your name is :' + str(len(myName)))
+#print(len(myName) ) 
+print('What is your age?')
+Myage=input()
+print('You will be ' + str(int(Myage)+1) + ' in a year')  
+#str(22) = '22' gia tri cua ham str
+#float('3.14') = 3.14 gia tri cua ham float
+#int('99.99') khong thuc hien duoc phai viet la int(99.99)
